66584d6d7fcdc725eea9d90068b34696589e6bb8
Nebulis <nebounet@gmail.com>
2020-01-31 18:24:20 +0800

    feat: export abi

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export {abi as TokenRegistryABI} from "../build/contracts/TradeTrustERC721.json";
+export {abi as TitleEscrowABI} from "../build/contracts/TitleEscrow.json";
 export * from "./token";
 export * from "./owner";
 export * from "./provider";
